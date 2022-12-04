3e5259d17bda2b02a76746d0f8d4ee52a3d7213c
choco boi <36957890+chocharrison@users.noreply.github.com>
2022-12-04 12:55:22 +0530

    Update part1.py

--- day4/part1.py
+++ day4/part1.py
@@ -1,18 +1,6 @@
 f = open("input.txt","r")
 pair = 0
 
-def sublist(ls1, ls2):
-    def get_all_in(one, another):
-        for element in one:
-            if element in another:
-                yield element
-
-    for x1, x2 in zip(get_all_in(ls1, ls2), get_all_in(ls2, ls1)):
-        if x1 != x2:
-            return False
-
-    return True
-
 for i in f:
   lisly = i.strip().split('-')
   lislis = lisly[1].split(',')
