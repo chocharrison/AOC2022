a40cf5851ef6c52d4d34f5b4a765f3a53de6fc18
choco boi <36957890+chocharrison@users.noreply.github.com>
2022-12-05 12:11:22 +0530

    Create part1.py

---
+++ day 5/part1.py
@@ -0,0 +1,25 @@
+import re
+f = open("input.txt","r")
+Dic = {}
+for i in range(1,10):
+  Dic[i] = []
+for j in f:
+  if (j == "\n"):
+    break
+  lis = [i for i, ltr in enumerate(j) if ltr == '[']
+  for k in lis:
+    ind = int(k/4+1)
+    Dic[ind] = [j[k+1]] + Dic[ind]
+print(Dic)
+for i in f:
+  lis = i.split()
+  num = int(lis[1])
+  src = int(lis[3])
+  des = int(lis[5])
+  for j in range(0,num):
+    Dic[des].append(Dic[src].pop())
+print(Dic)
+stri = ''
+for i in Dic:
+  stri += Dic[i][-1]
+print(stri)
