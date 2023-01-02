501a0c948b61afddb99a4b63005db97f10396121
choco boi <36957890+chocharrison@users.noreply.github.com>
2023-01-02 08:15:32 +0530

    Update part1.py

--- day 13/part1.py
+++ day 13/part1.py
@@ -1,38 +1,49 @@
-f = open("test",'r')
-comp = []
-
-def compare(left,right):
-  try:
-    y = -1
-    for i in range(len(left)):
-     # print(left[i],right[i])
-      if(type(left[i])==int and type(right[i])==int):
-        if(left[i] > right[i]):
-          return 0
-        elif(left[i] < right[i]):
-          return 1
-      elif(type(left[i])==list and type(right[i])==int):
-        y = compare(left[i],[right[i]])
-      elif(type(left[i])==int and type(right[i])==list):
-        y = compare([left[i]],right[i])
-      else:
-        y = compare(left[i],right[i])
-      if(y!= -1):
-        return y
-    if(len(left) < len(right)):
-      return 1
-  except:
-    return 0
+f = open("input.txt",'r')
+index = []
+lis = []
+ind = 1
+def check(lis1,lis2):
+  #print(lis1,lis2)
+  for i in range(len(lis1)):
+    try:
+      j = lis2[i]
+    except:
+      return 0
+    if(isinstance(lis1[i],int) and isinstance(lis2[i],int)):
+      if(lis1[i] < lis2[i]):
+        return 1
+      elif(lis1[i] > lis2[i]):
+        return 0
+    elif(isinstance(lis1[i],int) and isinstance(lis2[i],list)):
+      j = [lis1[i]]
+      j = check(j,lis2[i])
+      if(j==-1):
+        continue
+      return j
+    elif(isinstance(lis1[i],list) and isinstance(lis2[i],int)):
+      j = [lis2[i]]
+      j = check(lis1[i],j)
+      if(j==-1):
+        continue
+      return j
+    else:
+      j = check(lis1[i],lis2[i])
+      if(j==-1):
+        continue
+      return j
+  if(len(lis1) < len(lis2)):
+    return 1
   return -1
 
-
-count = 0
 for i in f:
   if(i=='\n'):
-    print(comp[0])
-    print(comp[1])
-    count+=compare(comp[0],comp[1])
-    comp = []
+    if(check(lis[0],lis[1])==1):
+      index.append(ind)
+    ind+=1
+    lis = []
   else:
-    comp.append(eval(i.strip()))
-print(count)
+    lis.append(eval(i.strip()))
+if(check(lis[0],lis[1])):
+  index.append(ind)
+print(index)
+print(sum(index))
